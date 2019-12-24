d838743a6157328fbc63a7aac337d73c6f6b58fb
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2019-12-24 19:17:11 -0200

    :sparkles: config gitflow
    add --no-ff to girflow feature finishes

--- scripts/install_utilities.sh
+++ scripts/install_utilities.sh
@@ -2,3 +2,4 @@
 
 # git flow
 wget -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; rm gitflow-installer.sh
+git config gitflow.feature.finish.no-ff yes --global
