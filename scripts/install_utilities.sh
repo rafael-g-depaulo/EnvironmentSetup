efc7f6e68f557ab979794f549d3d6004b9e1de18
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2019-12-26 23:03:30 -0200

    :mute: Remove logs from gitflow

--- scripts/install_utilities.sh
+++ scripts/install_utilities.sh
@@ -3,6 +3,6 @@
 # installing utilities
 
 # git flow
-wget -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; rm gitflow-installer.sh
-git config gitflow.feature.finish.no-ff yes --global
+wget -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable &> /dev/null; rm gitflow-installer.sh
+git config --global gitflow.feature.finish.no-ff yes
 git config --global --add merge.ff false
