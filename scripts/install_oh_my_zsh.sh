90136dd5b93fe7b1d79b9040cbb1ad38fce74384
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2019-12-26 23:05:28 -0200

    :mute: Remove logs from zsh setup

--- scripts/install_oh_my_zsh.sh
+++ scripts/install_oh_my_zsh.sh
@@ -4,8 +4,8 @@ source "$DIR/utils/install_package.sh"
 
 # INSTALL AND SETUP ZSH AND OH-MY-SZH
 # set up zsh and oh-my-zsh
-install_package zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y -qq
+install_package_silence zsh 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y -qq &> /dev/null
 
 # # powerline fonts for "agnoster" oh-my-zsh theme
 # sudo apt install -y -qq fonts-powerline
