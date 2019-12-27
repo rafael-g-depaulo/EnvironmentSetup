ff60a30f86e1820931c199e1fec7635230bc8b3e
Rafael G. de Paulo <rafael-g-depaulo@gmail.com>
2019-12-26 23:30:40 -0200

    :ambulance: Remove useless chmod

--- scripts/install_utilities.sh
+++ scripts/install_utilities.sh
@@ -4,7 +4,6 @@
 
 # git flow
 curl --silent --location  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh --output /tmp/gitflow-installer.sh
-chmod a+x /tmp/gitflow-installer.sh
 sudo bash /tmp/gitflow-installer.sh install stable &> /dev/null
 rm /tmp/gitflow-installer.sh
 
